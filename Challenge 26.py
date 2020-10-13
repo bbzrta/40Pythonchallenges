ca99bc04942c4b059b8226d04781295102bc159a
bbzrta <bbzrta@protonmail.com>
2020-10-14 00:58:24 +0100

    Challenge 26 completed.

--- Challenge 26.py
+++ Challenge 26.py
@@ -1,33 +1,27 @@
 # Factor Generator App
 print("Welcome to Factor Generator App!")
-user_num = int(input("what number would you like to see th factors for: "))
 
-end = False
+while True:
 
-while not end:
-
+    user_num = int(input("what number would you like to see the factors for: "))
     factors = []
 
     # Finding the factors of the given number and appending them to a list.
-    for i in range(1, user_num):
+    for i in range(1, user_num + 1):
         if user_num % i == 0:
             factors.append(i)
 
-    
     print("Factors of {0} are:".format(user_num))
-    
+
     for i in factors:
-        print(int(i))
+        print(i)
 
+    # Print a summary of the factors mathematically
+    print("\nIn summary: ")
+    for i in range(int(len(factors) / 2)):
+        print(str(factors[i]) + " * " + str(factors[-i - 1]) + " = " + str(user_num))
 
-
-
-
-
-
-
-
-
-    user_ender = input("would you like to try another number?")
-    if user_ender == "n":
-        end = True
+    if input("Run again (Y/n): ") == "n":
+        quit()
+    else:
+        continue
