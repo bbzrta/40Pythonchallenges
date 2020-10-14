8055b4c7c9e9aaa81c95c34610c0a3eb9dc33f44
bbzrta <bbzrta@gmail.com>
2020-10-14 17:15:54 +0100

    updated the names into two digits

--- Challenge 2.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to MPS to MPH convertor Program.")
-speed_miles = float(input("Please Enter yours speed in miles per hour: "))
-speed_meters = speed_miles * 0.44704
-speed_meters = round(speed_meters, 2)
-print("Your speed is " + str(speed_meters) + " MPS.")
