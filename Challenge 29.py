6082c5c6e8b6c8f5a23bb2bd4391b89ac2746b42
bbzrta <bbzrta@protonmail.com>
2020-10-20 18:12:08 +0100

    Guess the word App

--- Challenge 29.py
+++ Challenge 29.py
@@ -1,5 +1,11 @@
 # Guess The Word
 import random
+import os
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 print("Welcome to the guess my word app!")
 
@@ -8,7 +14,7 @@ game_dict = {
     "colours": ['orange', 'yellow', 'purple', 'black', 'red', 'blue'],
     "fruits": ['apple', 'peach', 'banana', 'pear', 'mango', 'strawberry'],
     "classes": ['english', 'history', 'science', 'mathematics', 'art'],
-            }
+}
 
 game_keys = []
 
@@ -16,13 +22,14 @@ for key in game_dict.keys():
     game_keys.append(key)
 
 while True:
-    category = game_keys[random.randint(0, len(game_dict)-1)]
-    word = game_dict[category][random.randint(0, len(game_dict[category])-1)]
+    category = game_keys[random.randint(0, len(game_dict) - 1)]
+    word = game_dict[category][random.randint(0, len(game_dict[category]) - 1)]
 
-    blank_word=[]
+    blank_word = []
     for letter in word:
         blank_word.append("-")
 
+    clear()
     print("\nguess a {0} letter word from the {1} category!".format(len(word), category))
 
     guess = ""
@@ -36,11 +43,13 @@ while True:
         if guess == word:
             print("\nCorrect! you guessed it after {0} tries!".format(guess_count))
         else:
+            clear()
             print("\nThat is incorrect. lets reveal a letter!")
             while True:
-                word_index = random.randint(0, len(word)-1)
+                word_index = random.randint(0, len(word) - 1)
                 blank_word[word_index] = word[word_index]
                 break
 
     if input("\nwould you like ot play again(Y/n): ").lower() == "n":
+        clear()
         quit("Thank you! Quitting...")
