3237da596b25fd00f19e606e23c6d1736ed48761
bbzrta <bbzrta@protonmail.com>
2020-10-26 23:05:27 +0000

    Power-Ball Simulator

---
+++ Challenge 30.py
@@ -0,0 +1,6 @@
+# Power-Ball Simulator app
+print("------------------------Power-Ball Simulator------------------------")
+
+white_balls_count = int(input("How many white-balls to from from for the 5 winning number(Normally 69): "))
+red_balls_count = int(input("How many red-balls to from from for the Power-Ball (Usually 26): "))
+
